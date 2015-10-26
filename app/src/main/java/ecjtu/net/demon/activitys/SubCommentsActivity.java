1e5a5e95bfe644a42a4d00821e8b1f8276636c43
shakugan <z18679760733@gmail.com>
2015-10-27 00:24:22 +0800

    添加意见反馈提交

--- app/src/main/java/ecjtu/net/demon/activitys/SubCommentsActivity.java
+++ app/src/main/java/ecjtu/net/demon/activitys/SubCommentsActivity.java
@@ -6,12 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 import ecjtu.net.demon.R;
+import ecjtu.net.demon.utils.HttpAsync;
+import ecjtu.net.demon.utils.ToastMsg;
 
 /**
+ * 意见反馈
  * Created by shakugan on 15/10/18.
  */
 public class SubCommentsActivity extends AppCompatActivity {
@@ -19,6 +30,7 @@ public class SubCommentsActivity extends AppCompatActivity {
     private EditText commentsText;
     private TextView sizeText;
     private Toolbar toolbar;
+    private Button submitBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,6 +59,28 @@ public class SubCommentsActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 sizeText.setText("还能输入"+(100-s.length())+"个字符");
+            }
+        });
+        submitBtn = (Button) findViewById(R.id.submitBtn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RequestParams params = new RequestParams();
+                params.add("username", NewMain.userEntity.getStudentID());
+                params.add("content",String.valueOf(commentsText.getText()));
+                HttpAsync.post("http://app.ecjtu.net/api/v1/feedbook",params,new JsonHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        ToastMsg.builder.display("提交成功,感谢您的反馈~~",500);
+                        finish();
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                        ToastMsg.builder.display("提交失败～～",500);
+                    }
+                });
             }
         });
     }
