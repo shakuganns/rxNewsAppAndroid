75c0d5809962fd547d6536299b28d12ef3790679
shakuganns <z18679760733@gmail.com>
2015-11-29 20:18:55 +0800

    add rxChat

---
+++ app/src/main/java/ecjtu/net/demon/adapter/ChatAdapter.java
@@ -0,0 +1,7 @@
+package ecjtu.net.demon.adapter;
+
+/**
+ * Created by Shakugan on 15/11/29.
+ */
+public class ChatAdapter {
+}
---
+++ app/src/main/java/ecjtu/net/demon/fragment/ChatFragment.java
@@ -0,0 +1,7 @@
+package ecjtu.net.demon.fragment;
+
+/**
+ * Created by Shakugan on 15/11/29.
+ */
+public class ChatFragment {
+}
